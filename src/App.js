71bdb538608a8d7612f94240c78858cae5ce4fc2
Nicholasmais <48137930+Nicholasmais@users.noreply.github.com>
2023-04-29 18:16:46 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  const teste  = [1,2,3,4];
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen  er:)</p>
-      <div className="Heelo">Teste</div>
+      <div className="Heelo">
+        {teste.map((i, val) => {
+          return (
+            <div key={i}>
+              {val}
+            </div>)
+        })}
+      </div>
     </div>
   );
 }
